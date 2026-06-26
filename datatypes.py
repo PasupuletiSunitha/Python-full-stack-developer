ce78ad7e7449e93892da297147fde5e79040e68c
Pasupuleti Sunitha <sunithapasupuleti501@gmail.com>
2026-06-26 20:42:04 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,46 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #datatypes
+>>> #integers
+>>> a=10
+>>> b=345
+>>> c=675
+>>> type(a)
+<class 'int'>
+>>> type(b)
+<class 'int'>
+>>> type(c)
+<class 'int'>
+>>> #float
+>>> e=4.5
+>>> f=7.8
+>>> type(e)
+<class 'float'>
+>>> type(f)
+<class 'float'>
+>>> #string
+>>> g="codegnan"
+>>> h="sunitha"
+>>> type(g)
+<class 'str'>
+>>> type(h)
+<class 'str'>
+>>> #complex
+>>> i=4+9j
+>>> k=3j+6
+>>> 7j
+7j
+>>> type(i)
+<class 'complex'>
+>>> type(k)
+<class 'complex'>
+>>> #boolean
+>>> a=True
+>>> A=True
+>>> B=False
+>>> type(A)
+<class 'bool'>
+>>> type(B)
+<class 'bool'>
+>>> #datatype conversions
+>>> #int
