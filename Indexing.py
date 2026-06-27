d25a97a2c3a682ca8f7df170b60da3493c6c2ae1
Pasupuleti Sunitha <sunithapasupuleti501@gmail.com>
2026-06-27 21:30:16 +0530

    Add files via upload

---
+++ Indexing.py
@@ -0,0 +1,39 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #indexing
+>>> #positive indexing
+>>> a="vijayawada"
+>>> a="i am in class"
+>>> a[8]+a[9]+a[10]+a[11]+a[12]
+'class'
+>>> a[5]+a[6]
+'in'
+>>> a[2]+a[3]
+'am'
+>>> b="i am learning python course"
+>>> b[1]
+' '
+>>> b[2]
+'a'
+>>> b[3]
+'m'
+>>> b[4]
+' '
+>>> b[5]
+'l'
+>>> b[6]
+'e'
+>>> b[7]
+'a'
+>>> b[8]
+'r'
+>>> b[9]
+'n'
+>>> b[10]
+'i'
+>>> b[5]+b[6]+b[7]+b[8]+b[9]+b[10]+b[11]+b[12]
+'learning'
+>>> #Negative indexing
+>>> b="vijayawada"
+>>> b[-10]+b[-9]+b[-8]+b[-7]+b[-6]+b[-5]
+'vijaya'
